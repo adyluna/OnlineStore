15b23df164d672b87c09776ecfa5cca68eb729b7
Izabella Cristina <oliveira_cavalcante_@hotmail.com>
2022-07-07 20:12:38 -0300

    Requisito 4 S2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,30 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Input from '../components/Input';
 // import ShoppingCart from './ShoppingCart';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categorie: [],
+    };
+  }
+
+  componentDidMount() {
+    this.showCategories();
+  }
+
+  showCategories = async () => {
+    const data = await getCategories();
+    this.setState({
+      categorie: data,
+    });
+  }
+
   render() {
     const myText = 'Digite algum termo de pesquisa ou escolha uma categoria.';
+    const { categorie } = this.state;
     return (
       <div>
         <Input
@@ -15,6 +35,16 @@ class Home extends Component {
           {myText}
         </h3>
         <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
+        <section>
+          { categorie.map((cat) => (
+            <button
+              type="button"
+              data-testid="category"
+              key={ cat.id }
+            >
+              { cat.name }
+            </button>))}
+        </section>
       </div>
     );
   }
