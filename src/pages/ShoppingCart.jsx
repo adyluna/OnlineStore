11a4a23c0021e002b391b0d906fe278b5ee72ddd
adyluna <adylunaleite@gmail.com>
2022-07-11 20:58:10 -0300

    Requisito 10 uhulll

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,93 +1,61 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import CartProduct from '../components/CartProduct';
 
 class ShoppingCart extends Component {
   constructor() {
     super();
-    this.state = { listProducts: [], listProductsInfo: [], quantity: 2 };
+    this.state = { listProducts: [], total: 0 };
   }
 
   componentDidMount() {
     this.getProducts();
-    this.getProductsInfos();
   }
 
   getProducts = () => {
-    const { addCart } = this.props;
-    this.setState({ listProducts: addCart });
+    const { products } = this.props;
+    this.setState({ listProducts: products });
   }
 
-  getProductsInfos = () => {
-    const { addInfo } = this.props;
-    this.setState({ listProductsInfo: addInfo });
+  // incrementQuantity = () => {
+  //   this.setState((state) => ({ quantity: state.quantity + 1 }));
+  // }
+
+  //   decrementQuantity = () => {
+  //     const { quantity } = this.state;
+  //     if (quantity > 1) {
+  //       this.setState((state) => ({ quantity: state.quantity - 1 }));
+  //     }
+  //   }
+
+  render() {
+    const message = 'Seu carrinho está vazio';
+    const { listProducts, total } = this.state;
+    const totalItens = `R$ ${total},00`;
+    return (
+      <div>
+        <Link to="/">Home</Link>
+        { listProducts.length === 0 ? (
+          <h3 data-testid="shopping-cart-empty-message">{ message }</h3>
+        )
+          : listProducts.map(({ title, thumbnail, price, id }) => (
+            <CartProduct
+              key={ id }
+              title={ title }
+              thumbnail={ thumbnail }
+              price={ price }
+              id={ id }
+            />
+          ))}
+        { totalItens }
+      </div>
+    );
   }
-
-  incrementQuantity = () => {
-    this.setState((state) => ({ quantity: state.quantity + 1 }));
-  }
-
-    decrementQuantity = () => {
-      const { quantity } = this.state;
-      if (quantity > 1) {
-        this.setState((state) => ({ quantity: state.quantity - 1 }));
-      }
-    }
-
-    render() {
-      const message = 'Seu carrinho está vazio';
-      const { listProducts, listProductsInfo, quantity } = this.state;
-      return (
-        <div>
-          <Link to="/">Home</Link>
-          { listProducts.length === 0 ? (
-            <h3 data-testid="shopping-cart-empty-message">{ message }</h3>
-          )
-            : listProducts.map(({ title, thumbnail, price, id }) => (
-              <div key={ id }>
-                <div>
-                  <p data-testid="shopping-cart-product-name">{title}</p>
-                  <img src={ thumbnail } alt={ title } />
-                  <p>{price}</p>
-                  <button
-                    type="submit"
-                    data-testid="product-increase-quantity"
-                    onClick={ this.decrementQuantity }
-                  >
-                    -
-
-                  </button>
-                  <p data-testid="shopping-cart-product-quantity">{quantity}</p>
-                  <button
-                    type="submit"
-                    data-testid="product-decrease-quantity"
-                    onClick={ this.incrementQuantity }
-                  >
-                    +
-
-                  </button>
-                </div>
-              </div>
-            ))}
-          <div>
-            <p data-testid="shopping-cart-product-name">{listProductsInfo.title}</p>
-            <img src={ listProductsInfo.thumbnail } alt={ listProductsInfo.title } />
-            <p>{listProductsInfo.price}</p>
-            <p data-testid="shopping-cart-product-quantity">
-              {
-                quantity === 1 ? null : quantity
-              }
-
-            </p>
-          </div>
-        </div>
-      );
-    }
 }
 
 ShoppingCart.propTypes = {
-  addCart: PropTypes.arrayOf.isRequired,
-  addInfo: PropTypes.arrayOf.isRequired,
+  products: PropTypes.arrayOf.isRequired,
 };
 
 export default ShoppingCart;
