aa2f5f9ef9685c991e9721e944b57d234f4b4d73
RafaAssis06 <rafa.assisn06@gmail.com>
2022-07-11 18:50:43 -0300

    [group 8] Req10

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 class ShoppingCart extends Component {
   constructor() {
     super();
-    this.state = { listProducts: [], listProductsInfo: [] };
+    this.state = { listProducts: [], listProductsInfo: [], quantity: 2 };
   }
 
   componentDidMount() {
@@ -23,34 +23,66 @@ class ShoppingCart extends Component {
     this.setState({ listProductsInfo: addInfo });
   }
 
-  render() {
-    const message = 'Seu carrinho está vazio';
-    const { listProducts, listProductsInfo } = this.state;
-    return (
-      <div>
-        <Link to="/">Home</Link>
-        { listProducts.length === 0 ? (
-          <h3 data-testid="shopping-cart-empty-message">{ message }</h3>
-        )
-          : listProducts.map(({ title, thumbnail, price, id }) => (
-            <div key={ id }>
-              <div>
-                <p data-testid="shopping-cart-product-name">{title}</p>
-                <img src={ thumbnail } alt={ title } />
-                <p>{price}</p>
-                <p data-testid="shopping-cart-product-quantity">01</p>
-              </div>
-            </div>
-          ))}
-        <div>
-          <p data-testid="shopping-cart-product-name">{listProductsInfo.title}</p>
-          <img src={ listProductsInfo.thumbnail } alt={ listProductsInfo.title } />
-          <p>{listProductsInfo.price}</p>
-          <p data-testid="shopping-cart-product-quantity">01</p>
-        </div>
-      </div>
-    );
+  incrementQuantity = () => {
+    this.setState((state) => ({ quantity: state.quantity + 1 }));
   }
+
+    decrementQuantity = () => {
+      const { quantity } = this.state;
+      if (quantity > 1) {
+        this.setState((state) => ({ quantity: state.quantity - 1 }));
+      }
+    }
+
+    render() {
+      const message = 'Seu carrinho está vazio';
+      const { listProducts, listProductsInfo, quantity } = this.state;
+      return (
+        <div>
+          <Link to="/">Home</Link>
+          { listProducts.length === 0 ? (
+            <h3 data-testid="shopping-cart-empty-message">{ message }</h3>
+          )
+            : listProducts.map(({ title, thumbnail, price, id }) => (
+              <div key={ id }>
+                <div>
+                  <p data-testid="shopping-cart-product-name">{title}</p>
+                  <img src={ thumbnail } alt={ title } />
+                  <p>{price}</p>
+                  <button
+                    type="submit"
+                    data-testid="product-increase-quantity"
+                    onClick={ this.decrementQuantity }
+                  >
+                    -
+
+                  </button>
+                  <p data-testid="shopping-cart-product-quantity">{quantity}</p>
+                  <button
+                    type="submit"
+                    data-testid="product-decrease-quantity"
+                    onClick={ this.incrementQuantity }
+                  >
+                    +
+
+                  </button>
+                </div>
+              </div>
+            ))}
+          <div>
+            <p data-testid="shopping-cart-product-name">{listProductsInfo.title}</p>
+            <img src={ listProductsInfo.thumbnail } alt={ listProductsInfo.title } />
+            <p>{listProductsInfo.price}</p>
+            <p data-testid="shopping-cart-product-quantity">
+              {
+                quantity === 1 ? null : quantity
+              }
+
+            </p>
+          </div>
+        </div>
+      );
+    }
 }
 
 ShoppingCart.propTypes = {
